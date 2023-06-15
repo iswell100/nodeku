c9662cdb2a5bcec1fee775a02beb55a5c6da81e8
iswell100 <wellsikandar100@gmail.com>
2023-06-15 15:25:28 +0700

    Update Method/Verb Request dico

---
+++ server.js
@@ -0,0 +1,40 @@
+const http = require('http');
+ 
+const requestListener = (request, response) => {
+    response.setHeader('Content-Type', 'text/html');
+    response.statusCode = 200;
+
+    const { method } = request;
+
+    if(method === 'GET') {
+        response.end('<h1>Hello!</h1>');
+    }
+ 
+    if(method === 'POST') {
+        response.end('<h1>Hai!</h1>');
+    }
+ 
+    if(method === 'PUT') {
+        response.end('<h1>Bonjour!</h1>');
+    }
+ 
+    if(method === 'DELETE') {
+        response.end('<h1>Salam!</h1>');
+    }
+
+    // Ini di bab *Membuat HTTP Server* 
+    // response.setHeader('Content-Type', 'text/html');
+ 
+    // response.statusCode = 200;
+    // response.end('<h1>Halo HTTP Server!</h1>');
+};
+ 
+ 
+const server = http.createServer(requestListener);
+
+const port = 9000;
+const host = 'localhost';
+ 
+server.listen(port, host, () => {
+    console.log(`Server berjalan pada http://${host}:${port}`);
+});
